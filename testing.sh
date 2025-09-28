9017cf00d0eaf7a150da4ebf226953a68d88738a
AmandeepBains <141843326+aman10000q@users.noreply.github.com>
2025-09-28 14:02:26 +0530

    Improve testing.sh with error handling and migration check
    Added error handling and enhanced database query output.

--- testing.sh
+++ testing.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+set -e
+
 echo "Testing the Database Connection:"
-PGPASSWORD=$PGPASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER_NAME -d orchestrator -t -c "select * from schema_migrations;"
+PGPASSWORD=$PGPASSWORD psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER_NAME" -d orchestrator -t -c "SELECT * FROM schema_migrations;"
+
+echo "Connection done"
+
+# Clone repo
+git clone --depth 1 https://$GIT_TOKEN@github.com/devtron-labs/devtron-enterprise -b develop
+cd devtron-enterprise/scripts/sql
+
+# Find highest migration number in repo
+latest_migration=$(ls | grep -E '^[0-9]+' | sed -E 's/^([0-9]+).*/\1/' | sort -n | tail -1)
+
+echo "Highest migration in repo: $latest_migration"
+
