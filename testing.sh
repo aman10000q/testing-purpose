bf67936f290884d1c721aeb3bf381a82c67b6096
AmandeepBains <141843326+aman10000q@users.noreply.github.com>
2025-10-04 11:53:15 +0530

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -5,55 +5,72 @@ echo "Testing the Database Connection:"
 PGPASSWORD=$PGPASSWORD psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER_NAME" -d orchestrator -t -c "SELECT * FROM schema_migrations;"
 echo "Connection done"
 
-# Clone repo
-migrationsToRunDown=0
-declare -A orchMigrationConfig
-orchMigrationConfig[repoUrl]="/devtron-labs/devtron-enterprise"
-orchMigrationConfig[branch]="$orchBranch"
-orchMigrationConfig[directory]="/devtron-enterprise/scripts/sql"
+# Define migration configs
+declare -A orchMigrationConfig=(
+  [repoUrl]="devtron-labs/devtron-enterprise"
+  [branch]="$orchBranch"
+  [directory]="devtron-enterprise/scripts/sql"
+  [cloneDir]="orch"
+  [migToRunDown]=0
+)
 
-declare -A casbinMigrationConfig
-casbinMigrationConfig[repoUrl]="/devtron-labs/devtron-enterprise"
-casbinMigrationConfig[branch]="${casbinBranch}"
-casbinMigrationConfig[directory]="/devtron-enterprise/scripts/casbin"
+declare -A casbinMigrationConfig=(
+  [repoUrl]="devtron-labs/devtron-enterprise"
+  [branch]="$casbinBranch"
+  [directory]="devtron-enterprise/scripts/casbin"
+  [cloneDir]="casbin"
+  [migToRunDown]=0
+)
 
-declare -A gitSensorMigrationConfig
-gitSensorMigrationConfig[repoUrl]="/devtron-labs/devtron-services-enterprise"
-gitSensorMigrationConfig[branch]="${gitSensorBranch}"
-gitSesnsorMigrationConfig[directory]="/devtron-enterprise/git-sensor/scripts/sql"
+declare -A gitSensorMigrationConfig=(
+  [repoUrl]="devtron-labs/devtron-services-enterprise"
+  [branch]="$gitSensorBranch"
+  [directory]="devtron-enterprise/git-sensor/scripts/sql"
+  [cloneDir]="git"
+  [migToRunDown]=0
+)
 
-declare -A lensMigrationConfig
-lensMigrationConfig[repoUrl]="/devtron-labs/lens"
-lensMigrationConfig[branch]="${lensBranch}"
-lensMigrationConfig[directory]="/lens/scripts/sql"
+declare -A lensMigrationConfig=(
+  [repoUrl]="devtron-labs/lens"
+  [branch]="$lensBranch"
+  [directory]="lens/scripts/sql"
+  [cloneDir]="lens"
+  [migToRunDown]=0
+)
 
-migrationConfigs=(orchMigrationConfig casbinMigrationConfig gitSensorMigrationConfig lensMigrationConfig);
+# Array of names of associative arrays
+migrationConfigs=(orchMigrationConfig casbinMigrationConfig gitSensorMigrationConfig lensMigrationConfig)
 
-for config in "{$migrationConfig[@]}"; do
-  declare -n currentConfig="${config}"
-  for 
-  directoryForCloning="testing-$branch"
-  git clone --depth 1 https://$GIT_TOKEN@github.com/devtron-labs/devtron-enterprise -b $branch "$directoryForCloning"
-  
+# Loop through each config
+for configName in "${migrationConfigs[@]}"; do
+  declare -n currentConfig="$configName"
 
+  sourceOfTruthLatestMigration=0
+  targetLatestMigration=0
 
+  for branch in "$sourceOfTruth" "${currentConfig[branch]}"; do
+    directoryForCloning="testing-${currentConfig[cloneDir]}-$branch"
+    repoUrl="https://$GIT_TOKEN@github.com/${currentConfig[repoUrl]}"
 
+    echo "Cloning $repoUrl branch $branch into $directoryForCloning"
+    git clone --depth 1 "$repoUrl" -b "$branch" "$directoryForCloning"
 
+    pushd "$directoryForCloning/${currentConfig[directory]}" > /dev/null
 
+    latestMigration=$(ls | grep -E '^[0-9]+' | sed -E 's/^([0-9]+).*/\1/' | sort -n | tail -1)
 
+    if [[ "$branch" == "$sourceOfTruth" ]]; then
+      sourceOfTruthLatestMigration=$latestMigration
+    else
+      targetLatestMigration=$latestMigration
+      # Arithmetic must be outside double-parens when assigning
+      currentConfig[migToRunDown]=$((targetLatestMigration - sourceOfTruthLatestMigration))
+    fi
 
+    popd > /dev/null
+  done
 
-
-for branch in "${automationBranches[@]}"; do
-  directoryForCloning="testing-$branch"
-  git clone --depth 1 https://$GIT_TOKEN@github.com/devtron-labs/devtron-enterprise -b $branch "$directoryForCloning"
-  cd "${directoryForCloning}/devtron-enterprise/scripts/sql"
-  if [[ $branch=="$sourceOfTruth" ]]; then
-    migrationsToRunDown=$(ls | grep -E '^[0-9]+' | sed -E 's/^([0-9]+).*/\1/' | sort -n | tail -1)
-  else
-    migrationsToRunDown=(($migrationsToRunDown-$(ls | grep -E '^[0-9]+' | sed -E 's/^([0-9]+).*/\1/' | sort -n | tail -1)))
-  fi
+  echo "${currentConfig[cloneDir]}: Migrations to run down: ${currentConfig[migToRunDown]}"
 done
 
-echo "Highest migration in repo: $latest_migration"
 
