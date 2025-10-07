81f098a95d4323887ee24e141f9d023dc7f33d71
AmandeepBains <141843326+aman10000q@users.noreply.github.com>
2025-10-07 15:11:54 +0530

    Update deployImage.sh

--- deployImage.sh
+++ deployImage.sh
@@ -20,6 +20,10 @@ declare -A qaVmCdIds=(
   [qa-devtroncd-5]=0
   [qa-devtroncd-4]=0
 )
+declare -A qaVmEnvIds=(
+[qa-devtroncd-5]=0
+[qa-devtroncd-4]=0
+)
 declare -A microserviceImages=(
   [casbin]=""
   [dashboard]=""
@@ -32,26 +36,61 @@ declare -A microserviceImages=(
   [gitSensor]=""
 )
 
-getQaEnvIds() {
+getQaCdIds() {
   local appId=$1;
   local vmName=$2;
   local apiEndpoint="$baseUrl/app/cd-pipeline/$appId";
   local response;
   response=$(curl -s -H "Cookie: argocd.token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkFQSS1UT0tFTjpzdXBlci1hZG1pbi10b2tlbiIsInZlcnNpb24iOiIxIiwiaXNzIjoiYXBpVG9rZW5Jc3N1ZXIifQ.jS0Keid81Ix4c4uzE1T-RZonPQn2WTqax_FDlYRQJ5I" "$apiEndpoint")
   qaVmCdIds[$vmName]=$(echo "$response" | jq --arg envName "$vmName" -r '.result.pipelines[]? | select(.environmentName == $envName) | .id' 2>/dev/null || echo "")
-  
+}
+getQaEnvIds(){
+local envName=$1;
+local apiEndPoint="$baseUrl/env"
+local response;
+response=$(curl -s -H "Cookie: argocd.token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkFQSS1UT0tFTjpzdXBlci1hZG1pbi10b2tlbiIsInZlcnNpb24iOiIxIiwiaXNzIjoiYXBpVG9rZW5Jc3N1ZXIifQ.jS0Keid81Ix4c4uzE1T-RZonPQn2WTqax_FDlYRQJ5I" "$apiEndpoint");
+envId=$(echo $response | jq --arg env $envName -r '.result[] | select(.environment_name == envName) | .id')
 }
 triggerDeployment(){
   local envIdToTrigger=$1;
   local imageToDeploy=$2;
-  local apiEndpoint="$baseUrl/";
+  local apiEndpoint="$baseUrl/webhook/ext-ci/$envIdToTrigger";
   local response;
-  response=$(curl -s )
+  response=$(curl -s -H "Cookie: argocd.token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkFQSS1UT0tFTjpzdXBlci1hZG1pbi10b2tlbiIsInZlcnNpb24iOiIxIiwiaXNzIjoiYXBpVG9rZW5Jc3N1ZXIifQ.jS0Keid81Ix4c4uzE1T-RZonPQn2WTqax_FDlYRQJ5I" -X POST "$apiEndpoint" -d "{\"dockerImage\":\"$imageToDeploy\"}")
 }
+waitForNewResources() {
+  local appId=$1
+  local environmentId=$2
+  local maxRetries=5
+  local sleepSeconds=5
+  local apiEndPoint="$baseUrl/app/detail/resource-tree?app-id=$appId&env-id=$environmentId"
+  for ((i=1; i<=maxRetries; i++)); do
+    echo "Attempt $i/$maxRetries..."
+    response=$(curl -s -H "Cookie: argocd.token=YOUR_TOKEN" "$apiEndPoint")
+    status=$(echo "$response" | jq -r '
+      .result.resources[]
+      | select(.name | contains("qa-migrate"))
+      | select(.kind == "Job")
+      | .health.status // empty
+    ')
+    if [[ "$status" == "Healthy" ]]; then
+      echo "Resource is Healthy!"
+      return 0
+    fi
+    if [[ $i -eq $maxRetries ]]; then
+      echo "Resource did not become Healthy after $maxRetries attempts."
+      return 1
+    fi
+    sleep "$sleepSeconds"
+  done
+}
+
 
 for microserviceId in "${!microservicesAppIds[@]}"; do
   for vm in "${!qaVmCdIds[@]}"; do
-    getQaEnvIds ${microservicesAppIds[$microserviceId]} $vm
+    getQaCdIds ${microservicesAppIds[$microserviceId]} $vm
     triggerDeployment ${qaVmCdIds[$vm]} ${microserviceImages[$microserviceId]}
+    getQaEnvIds $vm
+    waitForNewResources
   done
 done
